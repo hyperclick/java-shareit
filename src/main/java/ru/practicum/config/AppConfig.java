6d9eefd3cc667039492099233de3a89057480b5a
hyperclick <forspam28@ya.ru>
2022-07-31 09:53:42 +0300

    delete config

--- src/main/java/ru/practicum/config/AppConfig.java
+++ src/main/java/ru/practicum/config/AppConfig.java
@@ -1,9 +1,9 @@
-package ru.practicum.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:/application.properties")
-public class AppConfig {
-}
+//package ru.practicum.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//
+//@Configuration
+//@PropertySource("classpath:/application.properties")
+//public class AppConfig {
+//}
